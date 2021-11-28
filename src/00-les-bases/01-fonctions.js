4fd5fe37f9dda9954889df3a145485a5521fecc1
meyer clement <meyclem@gmail.com>
2021-11-29 00:53:05 +0100

    add more function exercises

--- src/00-les-bases/01-fonctions.js
+++ src/00-les-bases/01-fonctions.js
@@ -19,6 +19,31 @@ function greetings(firstname) {
 }
 
 /**
+ * Définir une variable 'isEven', elle doit:
+ * - prendre un chiffre en paramètre
+ * - renvoyer 'true' si il est pair
+ * - renvoyer 'false' si il est impair
+ */
+const isEven = (number) => number % 2 === 0;
+
+/**
+ * Définir une fonction 'canIDrive', elle doit:
+ * - prendre un paramètre 'age'
+ * - renvoyer 'true' si 'age' est supérieur ou égal à 18
+ * - renvoyer 'false' si 'age' est inférieur à 18
+ */
+function canIDrive (age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/** Définir une fonction 'capitalize' qui met en majuscule la première lettre du string en paramètre */
+const capitalize = (word) => word[0].toUpperCase() + word.substring(1);
+
+/**
  * Définir une fonction 'add', elle doit:
  * - prendre 2 paramètres
  * - renvoyer le résultat de l'addition si exécutée avec des nombres, ex: add(1, 2) => 3
@@ -28,3 +53,29 @@ const add = (a, b) => typeof a === 'number' && !isNaN(a) &&
     typeof b === 'number' && !isNaN(b)
     ? a + b
     : NaN;
+
+/**
+ * Définir une fonction 'repeat', elle doit:
+ * - prendre deux paramètres, un string et un chiffre
+ * - afficher le string dans la console autant de fois le chiffre passé en argument
+ * ex: repeat("Je suis concole.logué 3 fois", 3)
+ */
+
+function repeat(string, x) {
+    for (let i = 1; i <= x; i++) {
+        console.log(string);
+    }
+}
+
+/**
+ * Définir une fonction 'getRandomNumber', elle doit:
+ * - prendre deux paramètres, 'min' et 'max'
+ * - retourner un nombre aléatoire compris entre min et max
+ * - provoquer une erreur si 'min > max'
+ * Documentation: 📚 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Error
+ */
+
+function getRandomNumber(min, max) {
+    if (min > max) throw new Error('Must be \'min < max\'');
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
--- tests/01-fonctions.test.js
+++ tests/01-fonctions.test.js
@@ -3,16 +3,18 @@ const { findInCode } = require(`./read-code`);
 const path = require(`path`);
 
 describe(`Les fonctions`, () => {
-    let fakeConsoleLog;
+    let fakeConsoleLog = jest.spyOn(console, `log`).mockImplementation((string) => logs += `${string}`);
     const filePath = path.resolve(__dirname, `../src/00-les-bases/01-fonctions.js`);
-    let logs;
+    let logs = ``;
     
     beforeEach(() => {
-        logs = ``;
-        fakeConsoleLog = jest.spyOn(console, `log`).mockImplementation((string) => logs += string);
+        fakeConsoleLog = jest.spyOn(console, `log`).mockImplementation((string) => logs += `${string}`);
     });
   
-    afterEach(() => fakeConsoleLog.mockRestore());
+    afterEach(() => {
+        logs = ``;
+        fakeConsoleLog.mockRestore();
+    });
 
     describe(`#sayHello`, () => {
         let sayHello;
@@ -28,7 +30,7 @@ describe(`Les fonctions`, () => {
         test(`'sayHello()' doit imprimer 'Hello World!' dans la console`, async () => {
             sayHello();
 
-            expect(logs).toEqual(`Hello World!`);
+            expect(logs).toMatch(`Hello World!`);
         });
     });
 
@@ -47,7 +49,71 @@ describe(`Les fonctions`, () => {
         test(`'greetings("Ellie")' doit imprimer 'Hello Ellie!' dans la console`, async () => {
             greetings(`Ellie`);
 
-            expect(logs).toEqual(`Hello Ellie!`);
+            expect(logs).toMatch(`Hello Ellie!`);
+        });
+    });
+
+    describe(`#isEven`, () => {
+        let isEven;
+        beforeAll(async () => {
+            await findInCode(`isEven`, filePath).then(({value}) => isEven = value);
+        });
+
+        test(`Définir une fonction 'isEven' qui prend un chiffre en paramètre`, async () => {
+            const parametersCount = isEven.length;
+            expect(parametersCount).toEqual(1);
+        });
+
+        test(`Retourne 'true' si le chiffre est impair`, async () => {
+            expect(isEven(2)).toBe(true);
+            expect(isEven(42)).toBe(true);
+            expect(isEven(1000)).toBe(true);
+            expect(isEven(2322)).toBe(true);
+        });
+
+        test(`Retourne 'false' si le chiffre est impair`, async () => {
+            expect(isEven(3)).toBe(false);
+            expect(isEven(43)).toBe(false);
+            expect(isEven(10001)).toBe(false);
+            expect(isEven(23221)).toBe(false);
+        });
+    });
+
+    describe(`#canIDrive`, () => {
+        let canIDrive;
+
+        beforeAll(async () => {
+            await findInCode(`canIDrive`, filePath).then(({value}) => canIDrive = value);
+        });
+
+        test(`Définir une fonction 'canIDrive' qui prend un 'age' en paramètre`, async () => {
+            const parametersCount = canIDrive.length;
+            expect(parametersCount).toEqual(1);
+        });
+
+        test(`Retourne 'true' si l'âge est supérieur ou égal à 18`, async () => {
+            expect(canIDrive(18)).toBe(true);
+            expect(canIDrive(99)).toBe(true);
+        });
+
+        test(`Retourne 'false' si l'âge est inférieur à 18`, async () => {
+            expect(canIDrive(17)).toBe(false);
+        });
+    });
+
+    describe(`#capitalize`, () => {
+        let capitalize;
+        beforeAll(async () => {
+            await findInCode(`capitalize`, filePath).then(({value}) => capitalize = value);
+        });
+
+        test(`Définir une fonction 'capitalize' qui prend un paramètre`, async () => {
+            const parametersCount = capitalize.length;
+            expect(parametersCount).toEqual(1);
+        });
+
+        test(`Met en majuscule la première lettre du string passé en argument et le retourne`, async () => {
+            expect(capitalize(`hello`)).toEqual(`Hello`);
         });
     });
     
@@ -63,15 +129,81 @@ describe(`Les fonctions`, () => {
             expect(parametersCount).toEqual(2);
         });
 
-        test(`Doit renvoyer l'addition si les arguments sont des nombres`, async () => {
+        test(`Retourne l'addition si les arguments sont des nombres`, async () => {
             expect(add(1, 2)).toEqual(3);
             expect(add(-4 , 2)).toEqual(-2);
         });
 
-        test(`Doit renvoyer 'NaN' si au moins l'un des arguments n'est pas un nombre`, async () => {
+        test(`Retourne 'NaN' si au moins l'un des arguments n'est pas un nombre`, async () => {
             expect(add(false, `plouf`)).toBe(NaN);
             expect(add(NaN, 1)).toBe(NaN);
             expect(add(`pas un nombre`, [])).toBe(NaN);
+        });
+    });
+    
+    describe(`#repeat`, () => {
+        let repeat;
+        beforeAll(async () => {
+            await findInCode(`repeat`, filePath).then(({value}) => repeat = value);
+        });
+
+
+        test(`Définir une fonction 'repeat' qui prend deux paramètres`, async () => {
+            const parametersCount = repeat.length;
+            expect(parametersCount).toEqual(2);
+        });
+
+        test(`repeat doit utiliser console.log autant de fois que le chiffre indiqué en paramètre`, () => {
+            repeat(`Hello`, 3);
+            
+            expect(logs).toMatch(/(Hello){3}/);
+            expect(fakeConsoleLog).toHaveBeenCalledTimes(3);
+        });
+    });
+
+    describe(`#getRandomNumber`, () => {        
+        let getRandomNumber;
+        let fakeMathRandom;
+
+        beforeAll(async () => {
+            await findInCode(`getRandomNumber`, filePath).then(({value}) => getRandomNumber = value);
+        });
+
+        beforeEach(() => fakeMathRandom = jest.spyOn(Math, `random`));
+
+        afterEach(() => fakeMathRandom.mockRestore());
+
+        test(`Définir une fonction 'getRandomNumber' qui prend deux paramètres, 'min' et 'max'`, async () => {
+            const parametersCount = getRandomNumber.length;
+            expect(parametersCount).toEqual(2);
+        });
+
+        test(`Utilise 'Math.random'`, () => {
+            getRandomNumber(1, 6);
+
+            expect(Math.random).toHaveBeenCalledTimes(1);
+        });
+
+        test(`Retourne un nombre alétoire compris entre min et max`, async () => {
+            fakeMathRandom.mockReturnValue(0);
+
+            expect(getRandomNumber(1, 6)).toBe(1);
+            expect(getRandomNumber(2, 49)).toBe(2);
+
+            fakeMathRandom.mockReturnValue(0.99999999999);
+
+            expect(getRandomNumber(1, 666)).toBe(666);
+            expect(getRandomNumber(2, 4999)).toBe(4999);
+        });
+
+        test(`Provoquer une erreur si 'min' est supérieur à 'max'`, async () => {
+            expect.assertions(1);
+            try {
+                getRandomNumber(2, 1);
+            } catch(error) {
+                expect(error.message).toBe(`Must be 'min < max'`);
+            }
+
         });
     });
 });
