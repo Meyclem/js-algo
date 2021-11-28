f3abbc8216b6e495c28b5ce733ddb5fdcc4a6497
meyer clement <meyclem@gmail.com>
2021-11-28 20:40:25 +0100

    refacto on helpers functions

--- tests/00-variables.test.js
+++ tests/00-variables.test.js
@@ -1,47 +1,47 @@
 /* eslint quotes: ["error", "backtick"] */
-const { findVariableInCode } = require(`./read-code`);
+const { findInCode } = require(`./read-code`);
 const path = require(`path`);
 
 describe(`Les variables`, () => {
     const filePath = path.resolve(__dirname, `../src/00-les-bases/00-variables.js`);
 
     test(`Définir une variable constante 'promo' avec la valeur "Zagreus"`, async () => {
-        const {value: promo, keyword} = await findVariableInCode(`promo`, filePath);
+        const {value: promo, keyword} = await findInCode(`promo`, filePath);
 
         expect(keyword).toBe(`const`);
         expect(promo).toBe(`Zagreus`);
     });
 
     test(`Définir une variable constante 'firstname' avec le string 'John'`, async () => {
-        const {value: firstname, keyword} = await findVariableInCode(`firstname`, filePath);
+        const {value: firstname, keyword} = await findInCode(`firstname`, filePath);
 
         expect(keyword).toBe(`const`);
         expect(firstname).toBe(`John`);
     });
 
     test(`Définir une variable modifiable 'age' avec le chiffre '42'`, async () => {
-        const {value: age, keyword} = await findVariableInCode(`age`, filePath);
+        const {value: age, keyword} = await findInCode(`age`, filePath);
 
         expect(keyword).toBe(`let`);
         expect(age).toBe(42);
     });
 
     test(`Définir une variable constante 'oneMoreYear' avec le chiffre '1'`, async () => {
-        const {value: oneMoreYear, keyword} = await findVariableInCode(`oneMoreYear`, filePath);
+        const {value: oneMoreYear, keyword} = await findInCode(`oneMoreYear`, filePath);
 
         expect(keyword).toBe(`const`);
         expect(oneMoreYear).toBe(1);
     });
     
     test(`Définir une variable modifiable 'happyBirthsday' avec la l'addition de age et oneMoreYear`, async () => {
-        const {value: happyBirthsday, keyword} = await findVariableInCode(`happyBirthsday`, filePath);
+        const {value: happyBirthsday, keyword} = await findInCode(`happyBirthsday`, filePath);
     
         expect(keyword).toBe(`let`);
         expect(happyBirthsday).toBe(43);
     });
 
     test(`Changer la valeur de 'age' change la valeur de 'happyBirthsday'`, async () => {
-        const { value: age } = await findVariableInCode(`happyBirthsday`, filePath, {
+        const { value: age } = await findInCode(`happyBirthsday`, filePath, {
             regex: /(let|const)(\s*?)age(\s*?)=(\s*?)([0-9]{2})(\s*?);/,
             string: `const age = 1;`
         });
@@ -50,7 +50,7 @@ describe(`Les variables`, () => {
     });
 
     test(`Définir une variable 'pets' contenant les strings 'Garfield', 'Félix', 'Rantaplan', 'Robert'`, async () => {
-        const {value: pets, keyword} = await findVariableInCode(`pets`, filePath);
+        const {value: pets, keyword} = await findInCode(`pets`, filePath);
 
         expect(keyword).toBe(`const`);
         expect(Array.isArray(pets)).toBe(true);
@@ -58,14 +58,14 @@ describe(`Les variables`, () => {
     });
 
     test(`Définir une variable constante 'garfield' avec la valeur du 1er élément de 'pets'`, async () => {
-        const {value: garfield, keyword} = await findVariableInCode(`garfield`, filePath);
+        const {value: garfield, keyword} = await findInCode(`garfield`, filePath);
 
         expect(keyword).toBe(`const`);
         expect(garfield).toBe(`Garfield`);
     });
 
     test(`Changer la valeur de 'pets[0]' change la valeur de 'garfield'`, async () => {
-        const { value: garfield } = await findVariableInCode(`garfield`, filePath, {
+        const { value: garfield } = await findInCode(`garfield`, filePath, {
             regex: /(let|const) garfield/,
             string: `pets[0] = "Le chat roux"; $&`
         });
@@ -74,14 +74,14 @@ describe(`Les variables`, () => {
     });
 
     test(`Définir une variable constante 'lastPet' avec la valeur du dernier élément de 'pets'`, async () => {
-        const {value: lastPet, keyword} = await findVariableInCode(`lastPet`, filePath);
+        const {value: lastPet, keyword} = await findInCode(`lastPet`, filePath);
 
         expect(keyword).toBe(`const`);
         expect(lastPet).toBe(`Robert`);
     });
 
     test(`Ajouter un élément à 'pets' change la valeur de 'lastPet'`, async () => {
-        const { value: lastPet } = await findVariableInCode(`lastPet`, filePath, {
+        const { value: lastPet } = await findInCode(`lastPet`, filePath, {
             regex: /(let|const) lastPet/,
             string: `pets.push("Nounours"); $&`
         });
@@ -90,19 +90,19 @@ describe(`Les variables`, () => {
     });
 
     test(`Définir une variable constante 'allPets' avec la valeur jointe des éléments de 'pets'`, async () => {
-        const {value: pets} = await findVariableInCode(`pets`, filePath);
+        const {value: pets} = await findInCode(`pets`, filePath);
         const expectedValue = pets.join(`, `);
-        const {value: allPets, keyword} = await findVariableInCode(`allPets`, filePath);
+        const {value: allPets, keyword} = await findInCode(`allPets`, filePath);
 
         expect(keyword).toBe(`const`);
         expect(allPets).toBe(expectedValue);
     });
 
     test(`Changer les éléments de 'pets' change la valeur de 'allPets'`, async () => {
-        const {value: pets} = await findVariableInCode(`pets`, filePath);
+        const {value: pets} = await findInCode(`pets`, filePath);
         pets.push(`Nounours`);
         const expectedValue = pets.join(`, `);
-        const { value: allPets } = await findVariableInCode(`allPets`, filePath, {
+        const { value: allPets } = await findInCode(`allPets`, filePath, {
             regex: /(let|const) allPets/,
             string: `pets.push("Nounours"); $&`
         });
@@ -111,7 +111,7 @@ describe(`Les variables`, () => {
     });
 
     test(`Définir la variable 'jon' avec les bonnes propriétés`, async () => {
-        const {value: jon, keyword} = await findVariableInCode(`jon`, filePath);
+        const {value: jon, keyword} = await findInCode(`jon`, filePath);
 
         expect(keyword).toBe(`const`);
         expect(typeof jon).toBe(`object`);
@@ -120,7 +120,7 @@ describe(`Les variables`, () => {
     });
 
     test(`Changer la valeur de 'age' change l'age de John`, async () => {
-        const { value: jon } = await findVariableInCode(`jon`, filePath, {
+        const { value: jon } = await findInCode(`jon`, filePath, {
             regex: /(let|const)(\s*?)age(\s*?)=(\s*?)([0-9]{2})(\s*?);/,
             string: `const age = 1;`
         });
--- tests/read-code.js
+++ tests/read-code.js
@@ -2,38 +2,47 @@ const { parse, visit } = require('recast');
 const fs = require('fs');
 
 /**
- * This function will return an object with the keyword used to declare the variable
+ * This function will return an object with the keyword used to declare the variable or function
  * and will also return its value.
- * @param {string} id - The name of the variable to look for in the code
+ * @param {string} id - The name of the variable or function to look for in the code
  * @param {string} filePath - The path of the file to parse
  * @param {Object} codeReplacement - parameters for the code replacement with `String.Prototype.Replace()`
  * @param {RegExp} codeReplacement.regex - Regex used for the replacement in the code
  * @param {string} codeReplacement.string - Will be inserted in place of the regex match 
- * @returns {Promise<{ value: unknown, keyword: string }>} a `Promise` resolving to the value of the variable
+ * @returns {Promise<{ value: unknown, keyword: string }>} a `Promise` resolving to the value of the variable or function. If value is a function, keyword will be `""`
  */
-const findVariableInCode = async (id, filePath, codeReplacement = null) => {
+const findInCode = async (id, filePath, codeReplacement = null) => {
     const code = await readCode(filePath).then(code =>  codeReplacement
         ? code.replace(codeReplacement.regex, codeReplacement.string)
         : code
     );
     const ast = parse(code);
-    let keyword;
+    
+    const declaration = {};
     
     visit(ast, {
         visitVariableDeclaration: function(node) {
             if (node.value.declarations[0].id.name === id) {
-                keyword = node.value.kind;
+                declaration.keyword = node.value.kind;
+                this.abort();
+            } else {
+                this.traverse(node);
+            }
+        },
+        visitFunctionDeclaration: function(node) {
+            
+            if (node.value.id.name === id) {
+                declaration.keyword = 'function';
                 this.abort();
             } else {
                 this.traverse(node);
             }
         }
     });
-
-    return {
-        keyword,
-        value: eval(`${code}; ${id};`),
-    };
+    
+    declaration.value = eval(`${code}; ${id};`);
+    
+    return declaration;
 };
 
 /**
@@ -63,6 +72,6 @@ function readCode(path) {
 }
 
 module.exports = {
-    findVariableInCode,
+    findInCode,
     readCode,
 };
