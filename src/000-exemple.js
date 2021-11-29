2becbb120f95b0ba4df113015247a7bdd5bbe34b
Clement MEYER <clementmeyer@mac-mini-de-clement.home>
2021-11-29 13:40:46 +0100

    add examples

---
+++ src/000-exemple.js
@@ -0,0 +1,17 @@
+/* ⚠️Ne pas toucher⚠️ *//* eslint no-unused-vars: 0 */
+
+/**
+ * TODO: Écris ton code en dessous de chaque commentaire 👇
+ */
+
+// Définir une variable constante nommée promo et lui attributer la valeur '"Zagreus"'
+const promo = 'Zagreus';
+
+// Définir une variable modifiable 'myNumber' avec la valeur '1'
+let myNumber = 1;
+
+// Afficher "Hello World!" dans la console
+console.log('Hello World!');
+
+// Afficher un chiffre aléatoire entre 0 et 1 dans la console avec Math.random
+console.log(Math.random());
